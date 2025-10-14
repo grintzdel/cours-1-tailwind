34e04f33eb1278b128ec20098f688e11833d067d
maoudin <mathis.oudin42@gmail.com>
2025-10-14 19:47:31 +0200

    feat: update main page component

--- src/features/home/page.tsx
+++ src/features/home/page.tsx
@@ -1,12 +1,15 @@
+import React from "react";
+
 import HeroSection from "@/modules/app/react/components/hero-section";
 import AboutSection from "@/modules/app/react/components/about-section";
-import React from "react";
+import ProjectSection from "@/modules/app/react/components/project-section";
 
 export default function HomePage() {
     return (
         <React.Fragment>
             <HeroSection />
             <AboutSection />
+            <ProjectSection />
         </React.Fragment>
     )
 }
