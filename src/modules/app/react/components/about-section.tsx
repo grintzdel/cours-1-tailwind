2e4f3bfb46a715d1f4f9502e967e76651c355a40
maoudin <mathis.oudin42@gmail.com>
2025-10-14 14:37:42 +0200

    feat: update about section layout

--- src/modules/app/react/components/about-section.tsx
+++ src/modules/app/react/components/about-section.tsx
@@ -1,9 +1,17 @@
 import AboutTitle from "@/modules/app/react/components/about-title";
+import AboutText from "@/modules/app/react/components/about-text";
+import AboutGrid from "@/modules/app/react/components/about-grid";
 
 export default function AboutSection() {
     return (
-        <section className="px-8 md:px-16">
-            <AboutTitle />
+        <section className="px-8 md:px-16 py-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <AboutTitle />
+                <div className="row-span-2">
+                    <AboutGrid />
+                </div>
+                <AboutText />
+            </div>
         </section>
     )
 }
