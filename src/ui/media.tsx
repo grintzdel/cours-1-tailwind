d12cc86fd3ac30cf99868ef550189a14a4bb6f74
maoudin <mathis.oudin42@gmail.com>
2025-10-14 14:37:13 +0200

    feat: update media component

--- src/ui/media.tsx
+++ src/ui/media.tsx
@@ -8,8 +8,14 @@ type MediaProps = {
     height: number;
 }
 
+type MediaSkeletonProps = {
+    width: number;
+    height: number;
+}
+
 type MediaGalleryProps = {
     children: React.ReactNode;
+    flexDirection?: 'col' | 'row';
 }
 
 export const Media = ({src, alt, width, height}: MediaProps): JSX.Element => {
@@ -18,9 +24,21 @@ export const Media = ({src, alt, width, height}: MediaProps): JSX.Element => {
     )
 }
 
-export const MediaGallery = ({children}: MediaGalleryProps): JSX.Element => {
+export const MediaSkeleton = ({width, height}: MediaSkeletonProps): JSX.Element => {
     return (
-        <div className="flex flex-col gap-4 lg:flex-row">
+        <div
+            className="rounded-md border-2 border-gray-300"
+            style={{width: `${width}px`, height: `${height}px`}}
+        />
+    )
+}
+
+export const MediaGallery = ({children, flexDirection = 'row'}: MediaGalleryProps): JSX.Element => {
+    const mobileClass = flexDirection === 'row' ? 'flex-col' : 'flex-row';
+    const desktopClass = flexDirection === 'row' ? 'lg:flex-row' : 'lg:flex-col';
+
+    return (
+        <div className={`flex ${mobileClass} gap-2 ${desktopClass}`}>
             {children}
         </div>
     )
