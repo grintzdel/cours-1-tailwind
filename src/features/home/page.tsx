3fef2fb9f6d90b7e980a03ea941f033f87b45ea0
maoudin <mathis.oudin42@gmail.com>
2025-10-14 13:43:05 +0200

    refactor: add fragment

--- src/features/home/page.tsx
+++ src/features/home/page.tsx
@@ -1,11 +1,12 @@
 import HeroSection from "@/modules/app/react/components/hero-section";
 import AboutSection from "@/modules/app/react/components/about-section";
+import React from "react";
 
 export default function HomePage() {
     return (
-        <>
+        <React.Fragment>
             <HeroSection />
             <AboutSection />
-        </>
+        </React.Fragment>
     )
 }
