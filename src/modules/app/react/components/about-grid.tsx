22c8778e021b69ea4ea2fba38a1accca6f8dc868
maoudin <mathis.oudin42@gmail.com>
2025-10-14 16:29:43 +0200

    fix: fix media width

--- src/modules/app/react/components/about-grid.tsx
+++ src/modules/app/react/components/about-grid.tsx
@@ -3,9 +3,9 @@ import {MediaGallery, MediaSkeleton} from "@/ui/media";
 export default function AboutGrid() {
     return (
         <MediaGallery flexDirection="col">
-            <MediaSkeleton width={100} height={70} />
-            <MediaSkeleton width={100} height={70} />
-            <MediaSkeleton width={100} height={70} />
+            <MediaSkeleton width={140} height={70} />
+            <MediaSkeleton width={140} height={70} />
+            <MediaSkeleton width={140} height={70} />
         </MediaGallery>
     )
 }
--- src/modules/app/react/components/hero-portfolio.tsx
+++ src/modules/app/react/components/hero-portfolio.tsx
@@ -7,12 +7,12 @@ export default function HeroPortfolio() {
     return (
         <div className="flex flex-wrap justify-center content-center py-16 flex-col gap-9 lg:flex-row lg:justify-between lg:gap-0">
             <div className="flex items-center">
-                <p className="uppercase text-white">See our portfolio</p>
+                <p className="uppercase">See our portfolio</p>
             </div>
             <MediaGallery>
-                <Media src="/media-1.jpg" alt="" width={100} height={70}/>
-                <Media src="/media-2.jpg" alt="" width={100} height={70}/>
-                <Media src="/media-3.jpg" alt="" width={100} height={70}/>
+                <Media src="/media-1.jpg" alt="" width={140} height={70}/>
+                <Media src="/media-2.jpg" alt="" width={140} height={70}/>
+                <Media src="/media-3.jpg" alt="" width={140} height={70}/>
             </MediaGallery>
             <Link href="#" className="uppercase flex gap-12 items-center">
                 Scroll Now
