ac10331dd435d35f3d11eda324ddfe107e126b8b
maoudin <mathis.oudin42@gmail.com>
2025-10-14 15:45:27 +0200

    feat: add gradient background

--- src/modules/app/react/components/hero-section.tsx
+++ src/modules/app/react/components/hero-section.tsx
@@ -1,11 +1,14 @@
 import HeroText from "@/modules/app/react/components/hero-text";
 import HeroPortfolio from "@/modules/app/react/components/hero-portfolio";
+import {Background} from "@/ui/background";
 
 export default function HeroSection() {
     return (
+        <Background isGradient={true}>
         <section className="flex flex-col px-8 md:px-16">
             <HeroText />
             <HeroPortfolio />
         </section>
+        </Background>
     )
 }
