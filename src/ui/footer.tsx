e9fd3407f6e53241e36d64afa6a56a9cccf18b0a
maoudin <mathis.oudin42@gmail.com>
2025-10-14 09:52:12 +0200

    feat: add partials

---
+++ src/ui/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <h1>hello from footer</h1>
+        </footer>
+    )
+}
---
+++ src/ui/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>hello from header</h1>
+        </header>
+    )
+}
