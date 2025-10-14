947dc9c226d0b43e89598e4abfa167bb3ec55c7a
maoudin <mathis.oudin42@gmail.com>
2025-10-14 16:43:49 +0200

    feat: add project section block

---
+++ src/modules/app/react/components/project-section.tsx
@@ -0,0 +1,11 @@
+import ProjectHead from "@/modules/app/react/components/project-head";
+import ProjectBento from "@/modules/app/react/components/project-bento";
+
+export default function ProjectSection() {
+ return (
+     <section>
+        <ProjectHead />
+        <ProjectBento />
+    </section>
+ )
+}
